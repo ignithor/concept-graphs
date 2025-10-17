fb0161c1a0b54c525c04c0e6e9e1d8105b3de488
Paul Pham Dang <paul.phamdang@gmail.com>
2025-10-17 12:01:01 +0900

    base config

--- conceptgraph/utils/vis.py
+++ conceptgraph/utils/vis.py
@@ -202,7 +202,7 @@ def vis_result_fast_on_depth(
     depth_image: np.ndarray, 
     detections: sv.Detections, 
     classes: list[str], 
-    color: Color | ColorPalette = ColorPalette.default(), 
+    color: Color | ColorPalette = ColorPalette.DEFAULT, 
     instance_random_color: bool = False,
     draw_bbox: bool = True,
 ) -> np.ndarray:
@@ -213,13 +213,9 @@ def vis_result_fast_on_depth(
     # annotate image with detections
     box_annotator = sv.BoxAnnotator(
         color = color,
-        text_scale=0.3,
-        text_thickness=1,
-        text_padding=2,
     )
     mask_annotator = sv.MaskAnnotator(
         color = color,
-        opacity=0.2,
     )
 
     if hasattr(detections, 'confidence') and hasattr(detections, 'class_id'):
@@ -245,7 +241,7 @@ def vis_result_fast_on_depth(
     annotated_image = mask_annotator.annotate(scene=depth_image.copy(), detections=detections)
     
     if draw_bbox:
-        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections)
     return annotated_image, labels
 
 
@@ -462,7 +458,7 @@ def vis_result_for_vlm(
     image: np.ndarray, 
     detections: sv.Detections, 
     labels: list[str], 
-    color: Color | ColorPalette = ColorPalette.default(), 
+    color: Color | ColorPalette = ColorPalette.DEFAULT, 
     draw_bbox: bool = True,
     thickness: int = 2,
     text_scale: float = 0.3,
@@ -497,7 +493,7 @@ def vis_result_fast(
     image: np.ndarray, 
     detections: sv.Detections, 
     classes: list[str], 
-    color: Color | ColorPalette = ColorPalette.default(), 
+    color: Color | ColorPalette = ColorPalette.DEFAULT, 
     instance_random_color: bool = False,
     draw_bbox: bool = True,
 ) -> np.ndarray:
@@ -508,9 +504,6 @@ def vis_result_fast(
     # annotate image with detections
     box_annotator = sv.BoxAnnotator(
         color = color,
-        text_scale=0.3,
-        text_thickness=1,
-        text_padding=2,
     )
     mask_annotator = sv.MaskAnnotator(
         color = color
@@ -539,7 +532,7 @@ def vis_result_fast(
     annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
     
     if draw_bbox:
-        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections)
     return annotated_image, labels
 
 def vis_result_slow_caption(image, masks, boxes_filt, pred_phrases, caption, text_prompt):
